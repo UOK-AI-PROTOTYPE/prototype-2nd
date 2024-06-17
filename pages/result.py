8ec353bd762564f9ff84a2fdbf346b9bdead9b96
Junwoo <steven700@naver.com>
2024-06-17 17:48:55 +0900

    feat: 결과값 추출 완료

--- pages/result.py
+++ pages/result.py
@@ -4,9 +4,6 @@ from streamlit_chat import message
 from utils import modal
 import re
 
-if "result" in st.session_state:
-    result = st.session_state.result
-
 def find_mbti(result):
     mbti_pattern = re.compile(r'MBTI\s*:\s*([A-Z]{4})')
     mbti_match = mbti_pattern.search(result)
@@ -15,16 +12,16 @@ def find_mbti(result):
     ratio_pattern = re.compile(r'([A-Z])\s*\(.*?\):\s*(\d+)%')
     ratios = {match.group(1): int(match.group(2)) for match in ratio_pattern.finditer(result)}
 
+    return mbti, ratios
+
+for data in st.session_state.participant:
+    mbti, ratios = find_mbti(data["result"])
+    st.markdown(f"{data['name']}님의 분석결과")
     st.markdown(f"MBTI: {mbti}")
     st.markdown("비율:")
     for key, value in ratios.items():
         st.markdown(f"{key}: {value}%")
-    
-    return mbti
-
-for data in st.session_state.user_data:
-    st.markdown(f"{data['name']}님의 분석결과")
-    find_mbti(data["result"])  
+     
 
 if st.button("다시 분석 하러가기", type="primary"):
     # st.session_state의 특정값들을 삭제해서 chat 페이지로 돌아갔을 때, 상태 초기화
